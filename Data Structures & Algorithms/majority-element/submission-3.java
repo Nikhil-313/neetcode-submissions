de5c3aa2f42cb76dcf6b6c2cb73f61c0efd7d3dd
Akkenapally Nikhil <akkenapallynikhil313@gmail.com>
2026-09-08 00:26:14 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Integer candidate=null;
+        int count=0;
+        for(int num:nums){
+            if(count==0){
+                candidate=num;
+                count=1;
+            }
+            else if(num==candidate){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
